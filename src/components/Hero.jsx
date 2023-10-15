a19ccc324cdede1a957f201c58c8350dbeb45ce9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-10-15 17:10:02 +0300

    Finished Contact Us

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,16 +12,21 @@ const Hero = () => {
     >
       <div className="md:w-[100%] w-[90%] mx-auto flex md:flex-row flex-col-reverse justify-center items-center">
         <div className="md:w-[46%] flex  flex-col gap-6">
-          <p className="text-4xl font-semibold">Creative & Digital Agency</p>
-          <p>
+          <p className="text-7xl playfair font-semibold">Mr Kerry Martin</p>
+          <p className="roboto">
             We design brand identities & digital interaction for
             industry-changing clients across the globe.
           </p>
           <p className="w-[100px] bg-white h-[1px]" />
-          <p>See some of our latest projects below.</p>
+          <p className="montserrat text-4xl">Explore my world</p>
         </div>
         <div className="md:w-[46%] h-[90%]">
-          <img src="/image2.jpg" className="h-[100%] w-[100%] rounded-full object-cover" />
+          <div className="bg-[#2B2B2B] h-[100%] w-[100%] p-2 rounded-b-full rounded-tr-full">
+            <img
+              src="/image2.jpg"
+              className="h-[100%] w-[100%] rounded-full object-cover"
+            />
+          </div>
         </div>
       </div>
     </div>
